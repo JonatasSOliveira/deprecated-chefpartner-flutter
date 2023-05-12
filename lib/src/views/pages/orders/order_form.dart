1a65228c651b6f28bfbfaead2964b035e27e857b
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2023-05-12 20:56:29 -0300

    Init products list in order form

--- lib/src/views/pages/orders/order_form.dart
+++ lib/src/views/pages/orders/order_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:chefpartner_mobile/src/controllers/product_controller.dart';
+import 'package:chefpartner_mobile/src/dtos/order_product_dto.dart';
+import 'package:chefpartner_mobile/src/dtos/product_dto.dart';
 
 class OrderForm extends StatefulWidget {
   static const String _routeName = '/order_form';
@@ -14,15 +17,41 @@ class OrderForm extends StatefulWidget {
 }
 
 class _OrderFormState extends State<OrderForm> {
+  final ProductController _productController = ProductController();
+  List<ProductDTO> _products = [];
+  List<OrderProductDTO> _orderProductsCart = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _productController.listAll().then((products) => _products = products);
+  }
+
+  void _removeProductFromCart(ProductDTO product) {}
+
+  void _addProductToCart(ProductDTO product) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Novo Pedido'),
       ),
-      body: const Center(
-        child: Text('Novo Pedido'),
-      ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+        Expanded(
+            child: ListView.builder(
+                itemCount: _products.length,
+                itemBuilder: (context, index) => ListTile(
+                    trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+                      IconButton(
+                          icon: const Icon(Icons.remove_circle),
+                          onPressed: () => _removeProductFromCart(_products[index])),
+                      IconButton(
+                          icon: const Icon(Icons.add_circle),
+                          onPressed: () => _addProductToCart(_products[index]))
+                    ]),
+                    title: Text(_products[index].getName()))))
+      ]),
     );
   }
 }
