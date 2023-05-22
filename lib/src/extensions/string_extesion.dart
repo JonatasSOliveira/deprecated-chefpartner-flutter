ea5e0fe9f09244d7ca3e4119e54f8399977a3c22
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2023-05-22 20:41:30 +0000

    Creat CNPJ Mask Text Input Formatter

--- lib/src/extensions/string_extesion.dart
+++ lib/src/extensions/string_extesion.dart
@@ -5,6 +5,67 @@ extension StringExtension on String {
 
   int currencyToInt() {
     return int.parse(
-        _removeNonCurrencyChars().replaceAll('.', '').replaceAll(',', ''));
+      _removeNonCurrencyChars().replaceAll('.', '').replaceAll(',', '')
+    );
+  }
+
+  String formatAsCnpj() {
+    final StringBuffer formattedValue = StringBuffer();
+
+    if (length < 3) {
+      formattedValue.write(this);
+      return formattedValue.toString();
+    }
+
+    formattedValue.write('${substring(0, 2)}.');
+
+    if (length < 6) {
+      formattedValue.write(substring(2));
+      return formattedValue.toString();
+    }
+
+    formattedValue.write('${substring(2, 5)}.');
+
+    if (length < 9) {
+      formattedValue.write(substring(5));
+      return formattedValue.toString();
+    }
+
+    formattedValue.write('${substring(5, 8)}/');
+
+    if (length < 13) {
+      formattedValue.write(substring(8));
+      return formattedValue.toString();
+    }
+
+    formattedValue.write('${substring(8, 12)}-');
+    formattedValue.write(substring(12));
+    return formattedValue.toString();
+  }
+
+  String formatAsCpf() {
+    final StringBuffer formattedValue = StringBuffer();
+
+    if (length < 4) {
+      formattedValue.write(this);
+      return formattedValue.toString();
+    }
+
+    formattedValue.write('${substring(0, 3)}.');
+    if (length < 7) {
+      formattedValue.write(substring(3));
+      return formattedValue.toString();
+    }
+
+    formattedValue.write('${substring(3, 6)}.');
+    if (length < 10) {
+      formattedValue.write(substring(6));
+      return formattedValue.toString();
+    }
+
+    formattedValue.write('${substring(6, 9)}-');
+    formattedValue.write(substring(9));
+
+    return formattedValue.toString();
   }
 }
--- lib/src/views/input_formatters/cnpj_mask_text_input_formatter.dart
+++ lib/src/views/input_formatters/cnpj_mask_text_input_formatter.dart
@@ -1,18 +1,16 @@
-import 'package:chefpartner_mobile/src/extensions/string_extesion.dart';
 import 'package:flutter/services.dart';
 
-class CpfCnpjMaskTextInputFormatter extends TextInputFormatter {
-  static const int _cpfLength = 11;
+import 'package:chefpartner_mobile/src/extensions/string_extesion.dart';
 
+class CnpjMaskTextInputFormatter extends TextInputFormatter {
+  
   @override
   TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
     final String text = oldValue.text.length == newValue.text.length
         ? newValue.text.substring(0, newValue.text.length - 1)
         : newValue.text;
     final String numericText = text.replaceAll(RegExp(r'[^0-9]'), '');
-    final String formattedText = numericText.length > _cpfLength
-        ? numericText.formatAsCnpj()
-        : numericText.formatAsCpf();
+    final String formattedText = numericText.formatAsCnpj();
 
     return newValue.copyWith(
       text: formattedText,
